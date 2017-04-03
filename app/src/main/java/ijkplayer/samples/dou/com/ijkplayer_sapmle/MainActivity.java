16011f82acbbf5e028ddc823c37b7c8a5b5eae37
dou <yingnan.dou@qiwo.mobi>
2017-04-03 18:09:56 +0800

    init

---
+++ app/src/main/java/ijkplayer/samples/dou/com/ijkplayer_sapmle/MainActivity.java
@@ -0,0 +1,82 @@
+package ijkplayer.samples.dou.com.ijkplayer_sapmle;
+
+import android.Manifest;
+import android.graphics.Path;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import ijkplayer.samples.dou.com.ijkplayer_sapmle.media.AndroidMediaController;
+import ijkplayer.samples.dou.com.ijkplayer_sapmle.media.IjkVideoView;
+import rx.functions.Action1;
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    IjkVideoView mVideoView;
+
+    TableLayout mHudView;
+
+    AndroidMediaController mMediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mVideoView = (IjkVideoView) findViewById(R.id.ijk_video_view);
+
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mMediaController = new AndroidMediaController(this, false);
+
+        mHudView = (TableLayout) findViewById(R.id.hud_view);
+    }
+
+    private void initData() {
+
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        if (aBoolean) {
+//                            String path = "/DCIM/Camera/VID_20170403_140043.mp4";
+//                            String path = "/Qiwo/58DF3A1D.avi";
+
+                            Uri net_url = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+
+                            mVideoView.setMediaController(mMediaController);
+                            mVideoView.setHudView(mHudView);
+//                            mVideoView.setVideoURI(net_url);
+                            mVideoView.setVideoURI(net_url);
+                            mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+                                @Override
+                                public void onPrepared(IMediaPlayer iMediaPlayer) {
+                                    mVideoView.start();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(MainActivity.this, "需要读取文件权限", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
